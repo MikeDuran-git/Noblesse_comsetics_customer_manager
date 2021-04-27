cfe54e39fcf67fec54b2af79a19e2c4cb707fa9b
MikeDuran-git <mike.duran@hotmail.de>
2021-04-27 18:17:33 +0200

    basic structure build

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<title>Noblesse Cosmetics Client Page</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+<link rel="stylesheet" href="./index.css">
+
+</head>
+<body>
+
+<!--HEADER-->
+<header id="main_header">
+    <h1>Liste des Clients</h1>
+</header>
+<!--END_HEADER-->
+
+
+<!--CENTER-->
+<div class="container" id="main_center">
+    
+    <!--Searchbar-->
+    <div>
+    
+    <input type="text" placeholder="Search..">
+    <button>BUTTON</button>
+    
+    </div>
+    <!--END Searchbar-->
+
+    
+    <!--DB_content-->
+    <div class="container" id="DB_content">
+        <button>SELECT_CLIENT</button>
+    </div>
+    <!--END DB_content-->
+    
+</div>
+<!--END CENTER-->
+
+
+
+<!--FOOTER-->
+<footer id="main_footer">
+    <p>footer</p>
+</footer>
+<!--END_FOOTER-->
+
+</body>
+</html>
