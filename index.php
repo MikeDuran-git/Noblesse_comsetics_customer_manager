249417f430d4fc7e72d753e7151e171801aa325e
Mike Duran <mike.duran@hotmail.de>
2021-04-29 11:36:13 +0200

    database main page print done

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-<link rel="stylesheet" href="index_style.css">
+<link rel="stylesheet" href="index.css">
 
 </head>
 <body style="background-color: burlywood;">
 
 
 <!--DATABASE CONNECTION-->
-<?php
-include 'connexion.php';
-?>
+    <?php
+    include 'connexion.php';
+    ?>
 <!--DATABASE CONNECTION END-->
 
 
@@ -25,20 +25,26 @@ include 'connexion.php';
 
 
     <!--CENTER-->
-    <div id="main_center">
+    <div class="container" id="main_center">
         
         <!--Searchbar-->
         <div>  
             <form name="form" method="post">
+            
+                <button id="add_client_button">Ajouter un Client</button>
+                <button id="rm_client_button">Enlever un Client</button>
+                
                 <input id="search_bar" name="search" type="text" placeholder="Search..">
                 <input type="submit" name="submit">
+            
             </form>
+
         </div>
         <!--END Searchbar-->
-
+        
         
         <!--DB_content-->
-        <div>
+        <div id="client_list">
             <table>
                 <tr>
                     <th>Bouton selection</th>
@@ -52,7 +58,6 @@ include 'connexion.php';
     
                     if(isset($_POST['submit'])) {
                         $client_name= $_POST['search'];
-                        echo $client_name."<br>";
                         
                         //le cas si il n'y a pas de nom spécifié
                         if (strlen($client_name)==0){
@@ -60,7 +65,7 @@ include 'connexion.php';
                         }
                         else{
                         //le cas si il y a un nom donner
-                            $result= $db->prepare("SELECT * FROM `clients` WHERE nom='$client_name'");
+                            $result= $db->prepare("SELECT * FROM `clients` WHERE nom='$client_name' or prenom='$client_name'");
 
                         }
 
@@ -86,16 +91,34 @@ include 'connexion.php';
                             ';
                         }
                     }
+                else{
+                    $result= $db->prepare("SELECT * FROM `clients`");
+                    $result->setFetchMode(PDO:: FETCH_OBJ);
+                    $result->execute();
+                    while($row = $result->fetch()){
+                        echo '            
+                        <tr>
+                        <td>
+                            <button class="container">Select</button>
+                        </td>
+                        <td>'.
+                            $row->prenom. #prenom
+                        '</td>
+                        <td>'.
+                            $row->nom. #nom
+                        '</td>
+                        <td>'.
+                            $row->Email. # email
+                        '</td>
+                        ';
+                    }
+                }
                 ?>
                 <!--PHP input structure END-->
             </table>
         </div>
         <!--END DB_content-->
-        
-        <div>
-            <button id="add_client_button">Ajouter un Client</button>
-            <button id="rm_client_button">Enlever un Client</button>
-        </div>
+
 
     </div>
     <!--END CENTER-->
@@ -104,7 +127,6 @@ include 'connexion.php';
 
 <!--FOOTER-->
 <footer id="main_footer">
-<h1>Footer</h1>
 </footer>
 <!--END_FOOTER-->
 
