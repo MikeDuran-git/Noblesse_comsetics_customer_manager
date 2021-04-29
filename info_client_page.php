e41cd175272736d71b503f408c2a15b410741565
Mike Duran <mike.duran@hotmail.de>
2021-04-29 12:35:04 +0200

    making order in the dir

--- info_client_page.php
+++ info_client_page.php
@@ -4,13 +4,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-<link rel="stylesheet" href="info_client_page.css">
+<link rel="stylesheet" href="./layouts/info_client_page.css">
 
 </head>
 <body style="background-color: burlywood;">
 
 
-<!--DATABASE CONNECTION-->
+<!--DATABASE CONNECTION + GET CLIENT INFO-->
     <?php
     include 'connexion.php';
     session_start();
